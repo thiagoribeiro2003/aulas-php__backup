05ad8d8f3b5e2ef1408ebb0c73ea7bd953f6216e
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-04-19 08:53:44 -0300

    Adição comando de saída

--- 01-sintaxe.php
+++ 01-sintaxe.php
@@ -19,6 +19,7 @@
     // Geração de texto estruturado (com tags, atributos)
     echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP.</h2>";
     echo "<h2>Gerando <span class=\"destaque\">HTML</span> através do PHP.</h2>";
+    echo "Oi GitHub :)";
 ?>
 
 </body>
